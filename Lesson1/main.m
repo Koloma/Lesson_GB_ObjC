6149b492b283067dd6e6d41e7b739286f19e7117
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-06-17 18:18:45 +0200

    Initial Commit

---
+++ Lesson1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson1
+//
+//  Created by user197514 on 6/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
