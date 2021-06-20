272e1ffb6b83fa2b6c113f5486641afa4da408d3
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-06-20 10:43:38 +0200

    add Calculator and English check

--- Lesson1/main.m
+++ Lesson1/main.m
@@ -6,11 +6,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Calculator.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        CGFloat a = [Calculator calculate:Summ :10 :20];
+        CGFloat b = [Calculator calculate:Sub :10 :20];
+        CGFloat c = [Calculator calculate:Mult :10 :20];
+        CGFloat d = [Calculator calculate:Div :10 :20];
+        
+        NSLog(@"Result: \n a = %f, \n b = %f, \n c = %f, \n d = %f", a, b, c, d);
+        
+        
+        
+//        for (char c = 'a'; c <= 'z'; c++) {
+//            NSString *firstLetter = [NSString stringWithFormat:@"%c", c];
+//            NSInteger asciiCode = [firstLetter characterAtIndex:0];
+//            NSLog(@"%@ = %li",firstLetter, asciiCode);
+//        }
+
     }
     return 0;
 }
