1a4b9062ec754a9732fc9ee929230a15ee933507
Alexandr Kolomewskiy <koloma2@mail.ru>
2021-06-27 10:55:45 +0200

    lesson 4

--- Lesson1/main.m
+++ Lesson1/main.m
@@ -6,11 +6,32 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Student.h"
+
+
+
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        Student *stud = [[Student new] initWithParams:18 name:@"Paul" surname:@"Shmit"];
+        NSLog(@"%@", stud);
+        stud.increaseAgeOneYear;
+        NSLog(@"%@", stud);
+        NSLog(@"%@", stud.fullName);
         
-        return 0;
+        NSMutableArray *students = [NSMutableArray new];
+        
+        for (int i=0; i<10; i++) {
+            Student *student = [[Student new] initWithParams:18+i name:[Student randomNameWithLength:6] surname:[Student randomNameWithLength:8]];
+            [students addObject:student];
+        }
+        
+        for (Student *student in students) {
+            NSLog(@"%@", student);
+        }
+
     }
+    return 0;
 }
